40648d3060167a7751d8f806c1f5e8991989fb6e
AidenBow <AidenBow@users.noreply.github.com>
2019-11-06 20:18:28 -0800

    added shouts

--- api/server.js
+++ api/server.js
@@ -8,28 +8,6 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  Shoutouts.find()
-  .then(shoutouts => {
-    res.status(200).json(shoutouts);
-  })
-  .catch (error => {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
-  });
-});
-
-server.post('/', (req, res) => {
-  Shoutouts.add(req.body)
-  .then(shoutout => {
-    res.status(201).json(shoutout);
-  })
-  .catch (error => {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot add the shoutout' });
-  });
-});
-
 server.get('/', async (req, res) => {
   try {
     const shouts = await db('shouts');
@@ -54,5 +32,7 @@ server.post('/', async (req, res) => {
     res.status(500).json({ error: 'Cannot add the shout' });
   }
 });
+
+
 
 module.exports = server;
