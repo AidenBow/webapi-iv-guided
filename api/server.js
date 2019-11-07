e5363ebe5b1cea54394146830bd6877c0f8e2643
AidenBow <AidenBow@users.noreply.github.com>
2019-11-06 20:16:38 -0800

    added shouts

--- api/server.js
+++ api/server.js
@@ -30,4 +30,29 @@ server.post('/', (req, res) => {
   });
 });
 
+server.get('/', async (req, res) => {
+  try {
+    const shouts = await db('shouts');
+    const messageOfTheDay = process.env.MOTD || 'Hi World!!!';
+
+    res.status(200).json({motd: messageOfTheDay, shouts});
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the shouts' });
+  }
+});
+
+server.post('/', async (req, res) => {
+  try {
+    const [id] = await db('shouts').insert(req.body);
+    console.log(req.body);
+    const shouts = await db('shouts');
+
+    res.status(201).json(shouts);
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot add the shout' });
+  }
+});
+
 module.exports = server;
